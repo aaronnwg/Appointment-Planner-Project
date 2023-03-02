1c0e4cb1e882070d72bd9b0b059261de2807dd2c
Aaron <kesslerawkessler@gmail.com>
2023-03-02 15:25:17 -0700

    appoinment form is broken

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {ContactPicker} from './../contactPicker/ContactPicker';
 
 export const AppointmentForm = ({
   contacts,
@@ -19,26 +18,8 @@ export const AppointmentForm = ({
       .split("/");
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
-  const handleTitleChange = ({ target }) => {
-    const { name, value } = target;
-    setTitle(value);
-  };
-  const handleDateChange = ({ target }) => {
-    const { name, value } = target;
-    setDate(value);
-  };
-  const handleTimeChange = ({ target }) => {
-    const { name, value } = target;
-    setTime(value);
-  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Enter Title" value={title} onChange ={handleTitleChange} />
-      <ContactPicker contacts={contacts} setContact={setContact} />
-      <input type="date" placeholder="Enter date"  value={date} min={getTodayString} onChange={handleDateChange}   />
-      <input type="time" placeholder="Enter time"  value={time} onChange={handleTimeChange} />
-      <input type="submit" value="Submit" />
-    </form>
+    AppointmentForm
   );
 };
