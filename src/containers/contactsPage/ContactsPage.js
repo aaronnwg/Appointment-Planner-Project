772d57de52068049f4382eed136f05986c3e6ca2
Aaron <kesslerawkessler@gmail.com>
2023-03-02 10:20:16 -0700

    completed contact info consts and duplicate check

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -2,18 +2,28 @@ import React, {useState, useEffect} from "react";
 import {ContactForm} from '../../components/contactForm/ContactForm.js';
 import {TileList} from '../../components/tileList/TileList.js';
 
-export const ContactsPage = () => {
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
-
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [duplicateName, setDuplicateName] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if(duplicateName === false){
+      props.addContact(name,phone,email);
+      setName('');
+      setPhone('');
+      setEmail('');
+
+    }
   };
 
   /*
