1e2c9d7135b819825606ff1ea09baaf9c594f1ad
Aaron <kesslerawkessler@gmail.com>
2023-03-27 15:33:18 -0600

    potential deployment solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+const redirectUri = 'http://tasty-laborer.surge.sh';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
